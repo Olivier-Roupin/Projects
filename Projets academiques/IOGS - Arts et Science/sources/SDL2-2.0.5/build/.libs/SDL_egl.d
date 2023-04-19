e20f20e9bf4d67ee622bfbe3bbab30735ae95ade
Olivier-Roupin <olivier.roupin@free.fr>
2023-04-20 00:04:28 +0200

    removing outsider code

--- Projets academiques/IOGS - Arts et Science/sources/SDL2-2.0.5/build/.libs/SDL_egl.d
+++
@@ -1,6 +0,0 @@
-build/SDL_egl.lo: /home/projet/SDL2-2.0.5/src/video/SDL_egl.c \
- /home/projet/SDL2-2.0.5/src/video/../SDL_internal.h \
- /home/projet/SDL2-2.0.5/src/video/../dynapi/SDL_dynapi.h \
- /home/projet/SDL2-2.0.5/src/video/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h
--- Projets academiques/IOGS - Arts et Science/sources/SDL2-2.0.5/build/.libs/SDL_shaders_gles2.d
+++
@@ -1,7 +0,0 @@
-build/SDL_shaders_gles2.lo: \
- /home/projet/SDL2-2.0.5/src/render/opengles2/SDL_shaders_gles2.c \
- /home/projet/SDL2-2.0.5/src/render/opengles2/../../SDL_internal.h \
- /home/projet/SDL2-2.0.5/src/render/opengles2/../../dynapi/SDL_dynapi.h \
- /home/projet/SDL2-2.0.5/src/render/opengles2/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h
